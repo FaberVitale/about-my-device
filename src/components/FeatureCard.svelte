7ac2f7ede19a09a8b4b125cf9f4360395cd20870
FaberVitale <faber.vitale@gmail.com>
2020-12-27 19:34:33 +0100

    feat: add anchors

--- src/components/FeatureCard.svelte
+++ src/components/FeatureCard.svelte
@@ -2,14 +2,45 @@
   article {
     margin-bottom: 1.5rem;
     overflow-x: hidden;
+    position: relative;
+  }
+
+  .anchor {
+    position: absolute;
+    top: 6px;
+    right: 16px;
+    outline: none;
+    color: var(--primary);
+
+    &:active {
+      color: var(--secondary);
+    }
+
+    &:not(:active) {
+      background: none;
+    }
+  }
+
+  heading {
+    display: flex;
+    align-items: baseline;
+
+    & > *:first-child {
+      margin-right: 0.3rem;
+    }
   }
 </style>
 
 <script lang="ts">
+  import slugify from 'slugify';
   export let id: string;
+
+  const normalizedId = slugify(id);
+  const href = `#${normalizedId}`;
 </script>
 
-<article class="paper border border-primary" id="{id}">
+<article class="paper border border-primary" id="{normalizedId}">
+  <a class="anchor" href="{href}">#</a>
   <heading>
     <slot name="heading" />
   </heading>
